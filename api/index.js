5b9972ccc8e0cdbc5c1b129128ea43fed4adbf45
Luciano Bustos <bustosluciano89@gmail.com>
2023-01-24 09:24:53 -0300

    upgrad API whit Next Match

--- api/index.js
+++ api/index.js
@@ -1,20 +1,32 @@
 import { Hono } from 'hono'
 import { serveStatic } from 'hono/serve-static.module'
 import leadboard from '../db/leaderboard.json'
+import nextmatch from '../db/nextmatch.json'
+
 
 const app = new Hono()
 
-app.get('/', (ctx) => ctx.json([
+app.get('/', (ctx) =>
+	ctx.json([
 		{
 			endpoint: '/leaderboard',
 			description: 'Returns the leaderboard'
-		}
-
+		},
+		{
+			endpoint: '/nextmatch',
+			description: 'Returns the next match to Belgrano'
+		}	
 	]))
+
 
 app.get('/leaderboard' , (ctx) => {
 	return ctx.json(leadboard)
 })
+
+app.get('/nextmatch' , (ctx) => {
+	return ctx.json(nextmatch)
+})
+
 
 app.get('/static/*', serveStatic({ root: './'}))
 
