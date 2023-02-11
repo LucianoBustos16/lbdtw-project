e1bb4ea51ee5bf263f65b65231cf9ac6690833fb
Luciano Bustos <bustosluciano89@gmail.com>
2023-02-11 11:22:54 -0300

    Create Team service

--- src/services/teams.js
+++ src/services/teams.js
@@ -1,3 +1,11 @@
 export const getAllTemas = async () => {
-    const response = fetch()
+    try {
+        const response = fetch('https://api.lbdtwplus.com.ar/teams')
+        const teams = await response.json()
+        return teams
+    }
+    catch (e) {
+        // Enviar el error a un servicio de reporte de error
+        return []
+    }
 }
