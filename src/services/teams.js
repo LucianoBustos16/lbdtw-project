01e43a5bbf69c1d331af5cab71daab9e7cdeff04
Luciano Bustos <bustosluciano89@gmail.com>
2023-02-11 15:05:57 -0300

    Add await in fetch

--- src/services/teams.js
+++ src/services/teams.js
@@ -1,6 +1,6 @@
 export const getAllTemas = async () => {
     try {
-        const response = fetch('https://api.lbdtwplus.com.ar/teams')
+        const response = await fetch('https://api.lbdtwplus.com.ar/teams')
         const teams = await response.json()
         return teams
     }
