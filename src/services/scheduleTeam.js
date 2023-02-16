d31236baaec086dbda51162826d0f33a7bfa8dbe
Luciano Bustos <bustosluciano89@gmail.com>
2023-02-16 20:01:29 -0300

    Ajustes

--- src/services/scheduleTeam.js
+++ src/services/scheduleTeam.js
@@ -1,18 +1,3 @@
-// //Nombre del equipo a buscar
-// const teamName = 'Belgrano'
-
-// //Traer los datos de la API
-// const response = await fetch('https://api.lbdtwplus.com.ar/teams')
-//   .then(response => {
-//     //Filtrar solo los partidos que incluyen al equipo buscado
-//     const filteredMatches = response.data.filter(match => match.team.localName === teamName || match.team.visitorName === teamName)
-    
-//     //Mostrar los partidos filtrados
-//     console.log(filteredMatches)
-//   })
-//   .catch(error => {
-//     console.error(error)
-//   })
   
 export const getAllMatch = async () => {
 	try {
