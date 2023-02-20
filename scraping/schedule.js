784720552a2951d30363e9602273b78426b2c37a
Luciano Bustos <bustosluciano89@gmail.com>
2023-02-20 20:09:24 -0300

    Add matchDate

--- scraping/schedule.js
+++ scraping/schedule.js
@@ -16,7 +16,7 @@ const teamId = {
 	'3579': 'Platense',
 	'9280': 'Barracas-Central',
 	'570': 'Velez',
-	'8629': 'Sarmiento',	
+	'8629': 'Sarmiento',
 	'1095': 'Newells',
 	'540': 'Boca',
 	'1243': 'Independiente',
@@ -41,7 +41,7 @@ const SELECTORS = {
 	localsImages: '.local img',
 	scores: '.resultado',
 	visitants: '.visitante span',
-	visitantsImages:  '.visitante img'	
+	visitantsImages: '.visitante img'
 }
 
 export async function getSchedule($) {
@@ -63,24 +63,23 @@ export async function getSchedule($) {
 		const $results = $ronda.find(SELECTORS.scores)
 		const $hours = $ronda.find(SELECTORS.hour)
 		const $date = $ronda.find(SELECTORS.date)
-		
+
 		$results.each((index, result) => {
 			const scoreRaw = $(result).text()
 			const score = cleanText(scoreRaw)
-			
+
 			const hourRaw = $($hours[index]).text()
-			console.log(hourRaw)
 			const hour = hourRaw.replace(/\t|\n|\s:/g, '').trim()
 
 			const dateRaw = $($date[index]).text()
 			const date = dateRaw.replace(/\t|\n|\s:/g, '').trim()
 			const [dayNumber, monthNumber] = date.split('/')
 			const prefixDate = `2023-${monthNumber}-${dayNumber}`
-			
+
 			const matchDate = new Date(`${prefixDate} ${hour} GMT+1`)
 			console.log(matchDate)
-			
-			
+
+
 			// const localNameRaw = $($locals[index]).text()
 			// const localName = cleanText(localNameRaw)
 			const localImg = $($localsImages[index]).attr('src')
@@ -102,44 +101,46 @@ export async function getSchedule($) {
 			const minutes = "0" + hourAr.getMinutes()
 
 			const day = hourAr.getDate()
-			const months = ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct','Nov','Dic']
+			const months = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic']
 			const month = months[hourAr.getMonth()]
 
 			const formattedTime = hours + ':' + minutes.substr(-2)
 			const formattedDate = day + ' ' + month
-			
+
 
 			const getTeamFrom = ({ name }) => TEAMS.find(team => team.teamId === name)
 			const localTeam = TEAMS.find(team => team.id === localShortName)
 			const visitantTeam = TEAMS.find(team => team.id === visitantShortName)
 
-			const team = getTeamFrom ({ name: localTeam, name:visitantTeam  })
+			const team = getTeamFrom({ name: localTeam, name: visitantTeam })
 
-matches.push({
-  date: formattedDate,
-  timestamp,
-  hour: formattedTime === 'NaN:aN' ? score : formattedTime,
-  teams: [
-    {
-      id: localId,
-      name: team,
-      shortName: localShortName,
-      ...localTeam
-    },
-    {
-      id: visitantId,
-      name: team,
-      shortName: visitantShortName,
-      ...visitantTeam
-    }
-  ],
-  score
-})
+			matches.push({
+				date: formattedDate,
+				timestamp,
+				hour: formattedTime === 'NaN:aN' ? score : formattedTime,
+				matchDate: matchDate,
+				teams: [
+					{
+						id: localId,
+						name: team,
+						shortName: localShortName,
+						...localTeam
+					},
+					{
+						id: visitantId,
+						name: team,
+						shortName: visitantShortName,
+						...visitantTeam
+					}
+				],
+				score
+			})
 		})
 
-		schedule.push({ 
+		schedule.push({
 			fecha: index + 1,
-			matches })
+			matches
+		})
 	})
 
 	return schedule
