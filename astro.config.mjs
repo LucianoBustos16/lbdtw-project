61566b0a35c8e73a4fc11d1c2f4cb40c8b7a9e74
Luciano Bustos <bustosluciano89@gmail.com>
2023-02-23 15:56:20 -0300

    Add google analitycs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 
 // https://astro.build/config
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), partytown()]
 });
