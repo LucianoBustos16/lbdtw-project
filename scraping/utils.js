686314b4da87ad2301c1396aabfd9d741cb39aa1
Luciano Bustos <bustosluciano89@gmail.com>
2023-08-21 14:13:01 -0300

    remove console.log

--- scraping/utils.js
+++ scraping/utils.js
@@ -70,7 +70,6 @@ export async function scrapeAndSave(name){
     const $ = await scrape(url)
     const content = await scraper($)
     logSuccess(`[${name}] scraped successfully`)
-    console.log(url)
 
     logInfo(`Writting [${name}] to database...`)
 
