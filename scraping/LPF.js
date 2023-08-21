a377e9594fd4625c416c4abc47a1e60b8728c524
Luciano Bustos <bustosluciano89@gmail.com>
2023-08-21 14:06:24 -0300

    Ajusto parametros al nuevo formato de la Copa

--- scraping/LPF.js
+++ scraping/LPF.js
@@ -15,13 +15,16 @@ const LEADERBOARD_SELECTORS = {
 }
 
 export async function getLeaderBoardLPF ($) {
-    const $rows = $('table.tablesorter1 tbody tr')
+    const $rows = $('table#posiciones tbody tr')
 
     const getTeamFrom = ({ name }) => TEAMS.find(team => team.name === name) 
 
     const leaderBoardSelectorEntries = Object.entries(LEADERBOARD_SELECTORS)
 
+
     let LPF = []
+    let rankCounter = 1; // Inicializar el contador de rango
+
     $rows.each((index, el) => {
         const leaderBoardEntries = leaderBoardSelectorEntries.map(([key, { selector, typeOf }]) => {
           const rawValue = $(el).find(selector).text()
@@ -39,13 +42,17 @@ export async function getLeaderBoardLPF ($) {
         const team = getTeamFrom ({ name: teamName })
         
 
-        LPF.push(
-          {
-          rank: index + 1,
+        LPF.push({
+          rank: rankCounter, // Usar el contador de rango
             ...leaderBoardForTeam,
             team
-        }
-        )        
+        });     
+        
+         // Incrementar el contador de rango y reiniciarlo si llega a 14
+         rankCounter++;
+         if (rankCounter > 14) {
+             rankCounter = 1;
+         }        
         })
 
         return LPF
