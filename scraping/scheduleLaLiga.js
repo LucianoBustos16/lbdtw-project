e771e9960dc05585d31ace7d075b116d9f7d7799
Luciano Bustos <bustosluciano89@gmail.com>
2024-01-15 19:53:41 -0300

    remove console.log

--- scraping/scheduleLaLiga.js
+++ scraping/scheduleLaLiga.js
@@ -118,8 +118,6 @@ export async function getScheduleLaLiga($) {
 			
 
 			const team = getTeamFrom({ name: localTeam, name: visitantTeam })
-
-			console.log(localTeam)
 			
 			matches.push({
 				formattedDate: formattedDate,
